2a1f447952749deb35b58473564bf272780c67a0
Connor Lee <45024235+connorleee@users.noreply.github.com>
2021-03-24 14:05:39 -0700

    can add users to db and hash passwords

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,13 +1,13 @@
 const pool = require("../db");
+const bcrypt = require("bcryptjs");
 
 module.exports = {
   getAll: async (req, res) => {
-    const { firstName, lastName, phone, email, password, userAuth } = req.body;
-    const client = pool.connect();
+    const client = await pool.connect();
 
     try {
       const { rows } = await client.query(
-        "SELECT (first_name, last_name, phone, email, userAuth) FROM users"
+        "SELECT id, first_name, last_name, phone, email, user_authority FROM users"
       );
 
       res.json(rows);
@@ -17,5 +17,50 @@ module.exports = {
       await client.release();
     }
   },
-  addUser: async (req, res) => {},
+  addUser: async (req, res) => {
+    const { firstName, lastName, phone, email, password, userAuth } = req.body;
+    const client = await pool.connect();
+
+    //password encryption before adding to DB
+    const salt = bcrypt.genSaltSync(10);
+    const hash = bcrypt.hashSync(password, salt);
+
+    try {
+      const addUser = await client.query(
+        "INSERT INTO users (first_name, last_name, phone, email, password_hash, user_authority) VALUES ($1, $2, $3, $4, $5, $6)",
+        [firstName, lastName, phone, email, hash, userAuth]
+      );
+
+      res.json({
+        msg: `${firstName} ${lastName} successfully added to the db`,
+      });
+    } catch (err) {
+      console.log(
+        `Failed to add ${firstName} ${lastName} to the database: `,
+        "\n",
+        err
+      );
+    } finally {
+      await client.release();
+    }
+  },
+  getUser: async (req, res) => {
+    const { id } = req.params;
+    const client = await pool.connect();
+
+    try {
+      const {
+        rows,
+      } = await client.query(
+        "SELECT id, first_name, last_name, phone, email, user_authority, password_hash FROM users WHERE id = $1",
+        [id]
+      );
+
+      res.json(rows[0]);
+    } catch (err) {
+      console.log(`Failed to get user ${id}: `, "\n", err);
+    } finally {
+      await client.release();
+    }
+  },
 };
--- routes/api/user.js
+++ routes/api/user.js
@@ -2,6 +2,8 @@ const router = require("express").Router();
 const userController = require("../../controllers/userController");
 
 // Matches route with "/api/user/"
-router.route("/").get(userController.getAll);
+router.route("/").get(userController.getAll).post(userController.addUser);
+
+router.route("/:id").get(userController.getUser);
 
 module.exports = router;
