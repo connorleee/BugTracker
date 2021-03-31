86e47cc9b82cde399e0f1c71d57cae92b6705612
Connor Lee <45024235+connorleee@users.noreply.github.com>
2021-03-31 13:02:27 -0700

    argon template imported for front end

--- client/src/index.js
+++ client/src/index.js
@@ -1,25 +1,38 @@
+/*!
+
+=========================================================
+* Argon Dashboard React - v1.2.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/argon-dashboard-react
+* Copyright 2021 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/argon-dashboard-react/blob/master/LICENSE.md)
+
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 
-import "./assets/plugins/nucleo/css/nucleo.css";
+import "assets/plugins/nucleo/css/nucleo.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
-import "./assets/scss/argon-dashboard-react.scss";
+import "assets/scss/argon-dashboard-react.scss";
 
-import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
+import AdminLayout from "layouts/Admin.js";
+import AuthLayout from "layouts/Auth.js";
 
 ReactDOM.render(
   <BrowserRouter>
     <Switch>
-      <Route path="/" exact="true" render={(props) => <App {...props} />} />
+      <Route path="/admin" render={(props) => <AdminLayout {...props} />} />
+      <Route path="/auth" render={(props) => <AuthLayout {...props} />} />
+      <Redirect from="/" to="/admin/index" />
     </Switch>
   </BrowserRouter>,
   document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
