bef8528afec568dbdfc17580c64b3460bb97ce0e
Connor Lee <45024235+connorleee@users.noreply.github.com>
2021-03-31 14:37:05 -0700

    added projects page

--- client/src/routes.js
+++ client/src/routes.js
@@ -1,27 +1,11 @@
-/*!
-
-=========================================================
-* Argon Dashboard React - v1.2.0
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/argon-dashboard-react
-* Copyright 2021 Creative Tim (https://www.creative-tim.com)
-* Licensed under MIT (https://github.com/creativetimofficial/argon-dashboard-react/blob/master/LICENSE.md)
-
-* Coded by Creative Tim
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-*/
 import Index from "views/Index.js";
 import Profile from "views/examples/Profile.js";
-import Maps from "views/examples/Maps.js";
+// import Maps from "views/examples/Maps.js";
 import Register from "views/examples/Register.js";
 import Login from "views/examples/Login.js";
 import Tables from "views/examples/Tables.js";
 import Icons from "views/examples/Icons.js";
+import Projects from "views/Projects";
 
 var routes = [
   {
@@ -32,19 +16,26 @@ var routes = [
     layout: "/admin",
   },
   {
+    path: "/projects",
+    name: "Projects",
+    icon: "ni ni-collection text-red",
+    component: Projects,
+    layout: "/admin",
+  },
+  {
     path: "/icons",
     name: "Icons",
     icon: "ni ni-planet text-blue",
     component: Icons,
     layout: "/admin",
   },
-  {
-    path: "/maps",
-    name: "Maps",
-    icon: "ni ni-pin-3 text-orange",
-    component: Maps,
-    layout: "/admin",
-  },
+  // {
+  //   path: "/maps",
+  //   name: "Maps",
+  //   icon: "ni ni-pin-3 text-orange",
+  //   component: Maps,
+  //   layout: "/admin",
+  // },
   {
     path: "/user-profile",
     name: "User Profile",
