802384b959036d05ee177214a0d072bd28b338a5
Connor Lee <45024235+connorleee@users.noreply.github.com>
2021-04-06 18:15:25 -0700

    started working on selected issue table

--- client/src/views/Project.js
+++ client/src/views/Project.js
@@ -32,6 +32,9 @@ const Project = (props) => {
   const [projectIssues, setProjectIssues] = useState(null);
   const [isNewMemberOpen, setIsNewMemberOpen] = useState(false);
   const [isNewTicketOpen, setIsNewTicketOpen] = useState(false);
+  const [selectedIssue, setSelectedIssue] = useState({});
+
+  let selectedIssueId = "";
 
   let getProjectUsersUrl = `http://localhost:3001/api/userProjects/${projectId}`;
 
@@ -52,6 +55,8 @@ const Project = (props) => {
     }
     fetchData();
   }, [projectId, getProjectUsersUrl]);
+
+  useEffect(() => {}, [selectedIssueId]);
 
   if (projectData && projectTeam && projectIssues) {
     return (
@@ -188,6 +193,99 @@ const Project = (props) => {
                   <tbody>
                     {projectIssues.map((issue) => {
                       return (
+                        <tr key={issue.id} id={issue.id}>
+                          <th
+                            onClick={() => {
+                              console.log(issue.id);
+                              selectedIssueId = issue.id;
+                            }}
+                          >
+                            {/* <a href="#" onClick={(e) => e.preventDefault()}> */}
+                            <Media>{issue.title}</Media>
+                            {/* </a> */}
+                          </th>
+                          <td>{issue.description}</td>
+                          <td key={issue.user_id}>
+                            {issue.first_name} {issue.last_name}
+                          </td>
+                          <td className="text-right">
+                            <UncontrolledDropdown>
+                              <DropdownToggle
+                                className="btn-icon-only text-light"
+                                href="#pablo"
+                                role="button"
+                                size="sm"
+                                color=""
+                                onClick={(e) => e.preventDefault()}
+                              >
+                                <i className="fas fa-ellipsis-v" />
+                              </DropdownToggle>
+                              <DropdownMenu
+                                className="dropdown-menu-arrow"
+                                right
+                              >
+                                <DropdownItem
+                                  href="#pablo"
+                                  onClick={(e) => e.preventDefault()}
+                                >
+                                  Edit Issue
+                                </DropdownItem>
+                                <DropdownItem
+                                  href="#pablo"
+                                  onClick={(e) => e.preventDefault()}
+                                >
+                                  Remove Issue
+                                </DropdownItem>
+                              </DropdownMenu>
+                            </UncontrolledDropdown>
+                          </td>
+                        </tr>
+                      );
+                    })}
+                  </tbody>
+                </Table>
+              </Card>
+            </Col>
+          </Row>
+          <Row className="mt-5">
+            <Col xl="4">
+              <Card className="shadow">Selected Issue</Card>
+            </Col>
+            <Col xl="8">
+              <Card className="shadow">
+                <CardHeader>
+                  <Row className="align-items-center">
+                    <h3 className="mb-0">Issues</h3>
+                    <div className="col text-right">
+                      <Button
+                        color="primary"
+                        onClick={() => setIsNewTicketOpen(true)}
+                        size="sm"
+                      >
+                        New Ticket
+                      </Button>
+
+                      <Modal
+                        open={isNewTicketOpen}
+                        onClose={() => setIsNewTicketOpen(false)}
+                      >
+                        *New Ticket Form*
+                      </Modal>
+                    </div>
+                  </Row>
+                </CardHeader>
+                <Table className="align-items-center table-flush" responsive>
+                  <thead className="thead-light">
+                    <tr>
+                      <th scope="col">Ticket Title</th>
+                      <th scope="col">Description</th>
+                      <th scope="col">Ticket Author</th>
+                      <th scope="col" />
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {projectIssues.map((issue) => {
+                      return (
                         <tr key={issue.id}>
                           <th>
                             <a href="#" onClick={(e) => e.preventDefault()}>
@@ -235,14 +333,6 @@ const Project = (props) => {
                   </tbody>
                 </Table>
               </Card>
-            </Col>
-          </Row>
-          <Row className="mt-5">
-            <Col xl="3">
-              <Card className="shadow">Place Holder</Card>
-            </Col>
-            <Col xl="9">
-              <Card className="shadow">Selected Issue</Card>
             </Col>
           </Row>
         </Container>
