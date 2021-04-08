2ee17767d14b98f910b49382d7df61271da09c92
Connor Lee <45024235+connorleee@users.noreply.github.com>
2021-04-08 10:52:40 -0700

    formatted comments section

--- client/src/components/Tickets/SelectedTicket.js
+++ client/src/components/Tickets/SelectedTicket.js
@@ -1,5 +1,16 @@
 import React, { useState } from "react";
-import { Card, CardHeader, Row, Col, CardTitle, Button } from "reactstrap";
+import {
+  Card,
+  CardHeader,
+  Row,
+  Col,
+  CardTitle,
+  CardText,
+  Button,
+  ToastHeader,
+  Toast,
+  ToastBody,
+} from "reactstrap";
 import moment from "moment";
 import Modal from "../../components/Modal/Modal";
 
@@ -22,9 +33,9 @@ export default function SelectedTicket({
           "No ticket selected"
         ) : (
           <>
-            <Row>
+            <Row className=" p-2">
               <Col xl="6">
-                <Card className="shadow">
+                <Card className="shadow p-2">
                   <Row>
                     <Col xl="6">
                       <h2>Ticket: {selectedTicket.title}</h2>
@@ -56,42 +67,41 @@ export default function SelectedTicket({
               </Col>
               <Col xl="6">
                 <Card className="shadow">
-                  <Row>
-                    <h2>Comments</h2>
-                    <div className="col text-right">
-                      <Button
-                        color="primary"
-                        onClick={() => setIsCommentOpen(true)}
-                        size="sm"
-                      >
-                        New Comment
-                      </Button>
-                      <Modal
-                        open={isCommentOpen}
-                        onClose={() => setIsCommentOpen(false)}
-                      >
-                        *New comment form*
-                      </Modal>
-                    </div>
-                  </Row>
+                  <CardHeader>
+                    <Row className="align-items-center">
+                      <h2>Comments</h2>
+                      <div className="col text-right">
+                        <Button
+                          color="primary"
+                          onClick={() => setIsCommentOpen(true)}
+                          size="sm"
+                        >
+                          New Comment
+                        </Button>
+                        <Modal
+                          open={isCommentOpen}
+                          onClose={() => setIsCommentOpen(false)}
+                        >
+                          *New comment form*
+                        </Modal>
+                      </div>
+                    </Row>
+                  </CardHeader>
                   {comments ? (
                     comments.map((comment) => {
                       return (
-                        <Card key={comment.id}>
-                          <CardTitle className="text-muted mb-0">
-                            <span id={comment.author_id}>
-                              {comment.first_name} {comment.last_name}
+                        <Card body className="shadow m-2" key={comment.id}>
+                          <CardTitle tag="h5">
+                            <span id={comment.author_id} className="">
+                              {comment.first_name} {comment.last_name} -{" "}
                             </span>
-                          </CardTitle>
-                          <p>
-                            <span>
+                            <span className="h6">
                               {moment(comment.created_at).format(
                                 "MMMM Do YYYY, h:mm:ss a"
                               )}
-                            </span>
-                          </p>
-
-                          <span>{comment.comment}</span>
+                            </span>{" "}
+                          </CardTitle>
+                          <CardText>{comment.comment}</CardText>
                         </Card>
                       );
                     })
