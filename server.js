34fef8e0ab998a86a67902a5ff20bdf85291c019
Connor Lee <45024235+connorleee@users.noreply.github.com>
2021-05-17 14:39:47 -0700

    ready for deployment

--- server.js
+++ server.js
@@ -30,6 +30,10 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "client/build")));
 }
 
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client/build/index.html"));
+});
+
 app.listen(PORT, () => {
   console.log(`API Server now listening on port ${PORT}`);
 });
